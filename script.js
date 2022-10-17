92aa2f3584aec35f727e95238b0f67f26e204973
caernst13 <caleb_ernst@yahoo.com>
2022-10-17 17:45:01 -0400

    made the password display on the screen

--- script.js
+++ script.js
@@ -2,16 +2,16 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword ();
-  var passwordText = document.querySelector("#password");
+// function writePassword() {
+//   var password = generatePassword ();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
-}
+// }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", generatePassword);
+generateBtn.addEventListener("click", characterSelect);
 var seccurePassword = []
 var lower = 'qwertyuiopasdfghjklzxcvbnm'.split('');
 var upper = 'QWERTYUIOPASDFGHJKLZXCVBNM'.split('');
@@ -19,12 +19,12 @@ var numb = '1234567890'.split('');
 var spec = '!@#$%^&*()-=_+'.split('');
 var next = ''
 var characters = []
-function generatePassword() {
+function characterSelect() {
     var length = prompt('How many digits would you like your password to be?');
     console.log(length)
     if (length < 8 || length > 128) {
         alert("You did not select a valid length, please try again.");
-        generatePassword();
+        characterSelect();
     } else {
         var lowerCase = confirm('Would you like your password to include lower case letters?');
         var upperCase = confirm('Would you like your password to contain upper case letters?');
@@ -33,29 +33,35 @@ function generatePassword() {
     }
     if (!lowerCase && !upperCase && !numbers && !special) {
         alert("You must select yes on at least one of the previous questions");
-        generatePassword();
+        characterSelect();
     } else {
-        assemblePassword(length, lowerCase, upperCase, numbers, special) ;       
+        generatePassword(length, lowerCase, upperCase, numbers, special) ;       
     }
 }
 
-function assemblePassword(length, lowerCase, upperCase, numbers, special) {
+function generatePassword(length, lowerCase, upperCase, numbers, special) {
    if (lowerCase) {
     characters = characters.concat(lower)
    }
    if (upperCase) {
-    characters = characters.concat(upper)
+    characters = characters.concat(upper);
    }
    if (numbers) {
-    characters = characters.concat(numb)
+    characters = characters.concat(numb);
    }
    if (special) {
-    characters = characters.concat(spec)
+    characters = characters.concat(spec);
    }  
    console.log(characters)
     for (let i = 0; i < length; i++) {
     next = characters[Math.floor(Math.random() * characters.length)];
     seccurePassword.push(next);
     }
-    console.log(seccurePassword)
+    console.log(seccurePassword);
+    writePassword(seccurePassword);
+}
+
+function writePassword (seccurePassword) {
+    var passwordText = document.querySelector('#password')
+    passwordText.textContent = (seccurePassword)
 }
